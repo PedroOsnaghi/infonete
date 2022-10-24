34845bbfe1a37e7ccfda48df4c90dd07307640d0
Pedro Osnaghi <osnaghi.prog@gmail.com>
2022-10-24 17:39:15 -0300

    registro con estilos

--- public/js/register.js
+++ public/js/register.js
@@ -1,17 +1,28 @@
 (function () {
     var email = document.getElementById("email");
+    var msg = document.getElementById('mail-validate-msg');
 
     email.addEventListener('change', function (){
-        ajax();
+
+        verificarMail(this.value);
     });
 
 
-    function ajax(){
+    function verificarMail(value){
         $.ajax({
-            url: 'http://localhost/infonete/usuario/existeEmail?email=' + email.value,
+            url: 'http://localhost/infonete/usuario/existeEmail?email=' + value,
             type: 'GET',
             success: function (response){
-                console.log(response);
+
+                if(response.email){
+                   email.classList.add('invalid');
+                   msg.classList.remove('valid-feedback');
+                   msg.classList.add('invalid-feedback');
+                   msg.innerHTML='El email ya se encuentra registrado.';
+                   msg.style.display = 'block';
+                }else{
+                    console.log("disponible");
+                }
             }
         });
 
